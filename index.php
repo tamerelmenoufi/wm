7a938931b8fe6a7c401653a1790637647f9079a5
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2024-12-30 23:31:57 +0200

    Atualizacoes

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
             foreach($d as $i => $v){
                 $campos[] = "{$i} = '{$v}'";
             }    
-            echo $q = "insert into pre_cadastro_aluno set ".implode(",", $campos);
-            echo "<br>";
+            echo $q = "insert into pre_cadastro_aluno set ".implode(", ", $campos);
+            echo "<hr>";
         }
