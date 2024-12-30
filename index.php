99db3bcd9c1ab031872f4576d7a6faca583ed3b9
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2024-12-31 01:15:45 +0200

    Atualizacoes

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
         mysql_connect("mysql","root","SenhaDoBanco") or die("erro conexao");
         mysql_select_db("cieceja_ead") or die('erro banco');
 
+        $homologacao = "_h";
+
 
         $query = "select * from pre_cadastro_aluno_migra";
         $result = mysql_query($query);
         
         while($d = mysql_fetch_object($result)){
+            set_time_limit(90);
             $campos = [];
             foreach($d as $i => $v){
                 if($i != 'codigo'){
@@ -15,7 +18,9 @@
                 }
                 
             }    
-            echo $q = "insert into pre_cadastro_aluno set ".implode(", ", $campos);
+            echo $q = "insert into pre_cadastro_aluno{$homologacao} set ".implode(", ", $campos);
+            // mysql_query($q);
+            // $codigo_aluno = mysql_insert_id();
             echo "<br><br>";
 
 
@@ -24,6 +29,7 @@
             $query1 = "select * from matricula_migra where codigo_aluno = '{$d->codigo}'";
             $result1 = mysql_query($query1);
             while($d1 = mysql_fetch_object($result1)){
+            set_time_limit(90);
                 echo "<br><b>Matriculas</b><br>";
                 $campos = [];
                 foreach($d1 as $i => $v){
@@ -37,7 +43,9 @@
                     }
                     
                 }    
-                echo $q = " - insert into matricula set ".implode(", ", $campos);
+                echo $q = " - insert into matricula{$homologacao} set ".implode(", ", $campos);
+                // mysql_query($q);
+                // $codigo_matricula = mysql_insert_id();
                 echo "<br>";   
                 
                 //Entrar nas Provas
@@ -45,6 +53,7 @@
                 $query2 = "select * from provas_migra where codigo_matricula = '{$d1->codigo}'";
                 $result2 = mysql_query($query2);
                 while($d2 = mysql_fetch_object($result2)){
+                    set_time_limit(90);
                     echo "<br><b>Porvas</b><br>";
                     $campos = [];
                     foreach($d2 as $i => $v){
@@ -60,7 +69,9 @@
                         }
                         
                     }    
-                    echo $q = " . -- insert into provas set ".implode(", ", $campos);
+                    echo $q = " . -- insert into provas{$homologacao} set ".implode(", ", $campos);
+                    // mysql_query($q);
+                    // $codigo_prova = mysql_insert_id();
                     echo "<br>";  
                     
                     // Listando as questoes
@@ -68,6 +79,7 @@
                     $query3 = "select * from provas_perguntas_migra where codigo_prova = '{$d2->codigo}'";
                     $result3 = mysql_query($query3);
                     while($d3 = mysql_fetch_object($result3)){
+                        set_time_limit(90);
                         echo "<br><b>Porvas Perguntas</b><br>";
                         $campos = [];
                         foreach($d3 as $i => $v){
@@ -81,16 +93,13 @@
                             }
                             
                         }    
-                        echo $q = " . -- insert into provas set ".implode(", ", $campos);
+                        echo $q = " . -- insert into provas{$homologacao} set ".implode(", ", $campos);
+                        // mysql_query($q);
                         echo "<br>";  
                         
                         // Listando as questoes
     
                         
-                        
-        
-        
-        
         
                     }
                     
