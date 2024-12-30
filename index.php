3a6dfba9817146d1694e10122bf9db28ca1577bf
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2024-12-30 23:26:10 +0200

    Atualizacoes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         mysql_select_db("ciec_ead");
 
 
-        $query = "select * from pre_cadastro_aluno_migra";
+        echo $query = "select * from pre_cadastro_aluno_migra";
         $result = mysql_query($query);
         while($d = mysql_fetch_object($result)){
             echo $d->nome."<br>";
