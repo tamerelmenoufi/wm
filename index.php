707d8dc0fadba3dfa0d84973e6fa3ef91c730c2e
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2024-12-31 00:50:42 +0200

    Atualizacoes

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
 
 
             //Entrar nas matículas
-            echo "<br><b>Matriculas</b><br>";
+            
             $query1 = "select * from matricula_migra where codigo_aluno = '{$d->codigo}'";
             $result1 = mysql_query($query1);
             while($d1 = mysql_fetch_object($result1)){
-
+                echo "<br><b>Matriculas</b><br>";
                 $campos = [];
                 foreach($d1 as $i => $v){
                     if($i != 'codigo'){
@@ -41,11 +41,11 @@
                 echo "<br>";   
                 
                 //Entrar nas Provas
-                echo "<br><b>Porvas</b><br>";
+                
                 $query2 = "select * from provas_migra where codigo_matricula = '{$d1->codigo}'";
                 $result2 = mysql_query($query2);
                 while($d2 = mysql_fetch_object($result2)){
-    
+                    echo "<br><b>Porvas</b><br>";
                     $campos = [];
                     foreach($d2 as $i => $v){
                         if($i != 'codigo'){
