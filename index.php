31ff995fc3935d30049e3a37d9c0a49677570ce9
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2024-12-31 02:29:41 +0200

    Atualizacoes

--- index.php
+++ index.php
@@ -1,34 +1,24 @@
 <?php
-        exit();
+        //exit();
+
+        if(!$_GET['c']) exit();
+
         mysql_connect("mysql","root","SenhaDoBanco") or die("erro conexao");
         mysql_select_db("cieceja_ead") or die('erro banco');
 
-        $homologacao = false;
+        $homologacao = "_h";
 
 
-        $query = "select * from pre_cadastro_aluno_migra";
+        $query = "select * from pre_cadastro_aluno_migra where codigo = '{$_GET['c']}'";
         $result = mysql_query($query);
         
         while($d = mysql_fetch_object($result)){
             set_time_limit(90);
-            $campos = [];
-            foreach($d as $i => $v){
-                if($i != 'codigo'){
-                    $campos[] = "{$i} = '{$v}'";
-                }
-                
-            }    
-            echo $q = str_replace($de, $para,"insert into pre_cadastro_aluno{$homologacao} set ".implode(", ", $campos));
-            mysql_query($q);
-            $codigo_aluno = mysql_insert_id();
-            $de = ['[codigo_aluno]','[codigo_matricula]','[codigo_prova]'];
-            $para = [$codigo_aluno,$codigo_matricula,$codigo_prova];
-            echo "<br><br>";
-
+              echo "<br><br>";
 
             //Entrar nas matículas
             
-            $query1 = "select * from matricula_migra where codigo_aluno = '{$d->codigo}'";
+            $query1 = "select * from matricula_migra where codigo_aluno = '{$d->codigo}' and codigo not in (select codigo from matricula{$homologacao} where codigo_aluno = '{$d->codigo}')";
             $result1 = mysql_query($query1);
             while($d1 = mysql_fetch_object($result1)){
             set_time_limit(90);
@@ -37,61 +27,38 @@
                 foreach($d1 as $i => $v){
                     if($i != 'codigo'){
                         if($i == 'codigo_aluno'){
-                            $campos[] = "codigo_aluno = '[codigo_aluno]'";
+                            $campos[] = "codigo_aluno = '{$d->codigo}'";
                         }else{
                             $campos[] = "{$i} = '{$v}'";
                         }
-                        
                     }
                     
-                }    
-                echo $q = str_replace($de, $para,"insert into matricula{$homologacao} set ".implode(", ", $campos));
-                mysql_query($q);
-                $codigo_matricula = mysql_insert_id();
-                $de = ['[codigo_aluno]','[codigo_matricula]','[codigo_prova]'];
-                $para = [$codigo_aluno,$codigo_matricula,$codigo_prova];
-                echo "<br>";   
-                
-                //Entrar nas Provas
-                
-                $query2 = "select * from provas_migra where codigo_matricula = '{$d1->codigo}'";
-                $result2 = mysql_query($query2);
-                while($d2 = mysql_fetch_object($result2)){
-                    set_time_limit(90);
-                    echo "<br><b>Porvas</b><br>";
-                    $campos = [];
-                    foreach($d2 as $i => $v){
-                        if($i != 'codigo'){
-                            if($i == 'codigo_matricula'){
-                                $campos[] = "codigo_matricula = '[codigo_matricula]'";
-                            }else if($i == 'codigo_aluno'){
-                                $campos[] = "codigo_aluno = '[codigo_aluno]'";
-                            }else{
-                                $campos[] = "{$i} = '{$v}'";
-                            }
-                            
-                        }
-                        
-                    }    
-                    echo $q = str_replace($de, $para,"insert into provas{$homologacao} set ".implode(", ", $campos));
-                    mysql_query($q);
-                    $codigo_prova = mysql_insert_id();
-                    $de = ['[codigo_aluno]','[codigo_matricula]','[codigo_prova]'];
-                    $para = [$codigo_aluno,$codigo_matricula,$codigo_prova];
-                    echo "<br>";  
-                    
-                    // Listando as questoes
+                }
 
-                    $query3 = "select * from provas_perguntas_migra where codigo_prova = '{$d2->codigo}'";
-                    $result3 = mysql_query($query3);
-                    while($d3 = mysql_fetch_object($result3)){
+                if(!mysql_num_rows(mysql_query("select codigo from matricula{$homologacao} where ".implode(", ", $campos)))){
+
+                    echo $q = "insert into matricula{$homologacao} set ".implode(", ", $campos);
+                    mysql_query($q);
+                    $codigo_matricula = mysql_insert_id();
+                    $de = ['[codigo_matricula]'];
+                    $para = [$codigo_matricula];
+                    echo "<br>"; 
+                
+                
+                    //Entrar nas Provas
+                    
+                    $query2 = "select * from provas_migra where codigo_matricula = '{$d1->codigo}' and codigo not in (select codigo from provas{$homologacao} where codigo_matricula = '{$d1->codigo}')";
+                    $result2 = mysql_query($query2);
+                    while($d2 = mysql_fetch_object($result2)){
                         set_time_limit(90);
-                        echo "<br><b>Porvas Perguntas</b><br>";
+                        echo "<br><b>Porvas</b><br>";
                         $campos = [];
-                        foreach($d3 as $i => $v){
+                        foreach($d2 as $i => $v){
                             if($i != 'codigo'){
-                                if($i == 'codigo_prova'){
-                                    $campos[] = "codigo_prova = '[codigo_prova]'";
+                                if($i == 'codigo_matricula'){
+                                    $campos[] = "codigo_matricula = '[codigo_matricula]'";
+                                }else if($i == 'codigo_aluno'){
+                                    $campos[] = "codigo_aluno = '{$d->codigo}'";
                                 }else{
                                     $campos[] = "{$i} = '{$v}'";
                                 }
@@ -100,22 +67,54 @@
                             
                         }
 
-                        echo $q = str_replace($de, $para,"insert into provas_perguntas{$homologacao} set ".implode(", ", $campos));
-                        mysql_query($q);
-                        echo "<br>";  
-                        
-                        // Listando as questoes
-    
-                        
-        
-                    }
-                    
-    
-    
-    
-    
-                }
+                        if(!mysql_num_rows(mysql_query("select codigo from provas{$homologacao} where ".implode(", ", $campos)))){
 
+                            echo $q = str_replace($de, $para,"insert into provas{$homologacao} set ".implode(", ", $campos));
+                            mysql_query($q);
+                            $codigo_prova = mysql_insert_id();
+                            $de = ['[codigo_matricula]','[codigo_prova]'];
+                            $para = [$codigo_matricula,$codigo_prova];
+                            echo "<br>";  
+                            
+                            // Listando as questoes
+
+                            $query3 = "select * from provas_perguntas_migra where codigo_prova = '{$d2->codigo}' and codigo not in (select codigo from provas_perguntas{$homologacao} where codigo_prova = '{$d2->codigo}')";
+                            $result3 = mysql_query($query3);
+                            while($d3 = mysql_fetch_object($result3)){
+                                set_time_limit(90);
+                                echo "<br><b>Porvas Perguntas</b><br>";
+                                $campos = [];
+                                foreach($d3 as $i => $v){
+                                    if($i != 'codigo'){
+                                        if($i == 'codigo_prova'){
+                                            $campos[] = "codigo_prova = '[codigo_prova]'";
+                                        }else{
+                                            $campos[] = "{$i} = '{$v}'";
+                                        }
+                                        
+                                    }
+                                    
+                                }
+
+                                if(!mysql_num_rows(mysql_query("select codigo from provas_perguntas{$homologacao} where ".implode(", ", $campos)))){
+
+                                    echo $q = str_replace($de, $para,"insert into provas_perguntas{$homologacao} set ".implode(", ", $campos));
+                                    mysql_query($q);
+                                    echo "<br>";  
+
+                                }//fim do if para inserção das provas Perguntas
+                                
+                                // Listando as questoes
+            
+                                
+                
+                            }
+
+                        }//fim do if para inserção das provas
+
+                    }
+
+                }//fim do if para inserção das matriculas
 
             }
             echo "<hr>"; 
