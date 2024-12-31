2c2466661549953da16679738eaf9c92d72d786b
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2024-12-31 03:40:49 +0200

    Atualizacoes

---
+++ script.php
@@ -0,0 +1,8 @@
+<?php
+    // // URL para onde o GET será enviado
+    // $url = 'https://wm.ciec-eja.com.br/migracao/?c='.$_POST['c'];
+    // // Envia a requisição GET
+    // $resposta = file_get_contents($url);
+    // // Exibe a resposta
+    // echo $resposta;
+?>
